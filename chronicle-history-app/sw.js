40f8158a40cfa55b6e4d4a9a92f9bc002c0022ab
Elise Roberts <101602267+rivel5@users.noreply.github.com>
2026-08-12 10:34:13 -0400

    Move Chronicle to dedicated repository

--- chronicle-history-app/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='chronicle-v2';
-const FILES=['./','./index.html','./styles.css','./app.js','./lessons-bahamian.js','./lessons-caribbean.js','./lessons-world.js','./lessons-american.js','./manifest.webmanifest','./icons/icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
